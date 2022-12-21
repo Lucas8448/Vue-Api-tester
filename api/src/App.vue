75c7a1c34c59e60db10bc669cd5f5178bef389ee
Lucas Bateson <lucas.bateson@gmail.com>
2022-12-21 16:59:37 +0100

    V2 with custom query parameters

--- api/src/App.vue
+++ api/src/App.vue
@@ -4,6 +4,12 @@
 <template>
   <div class="container">
     <input v-model="apiEndpoint" type="text" placeholder="API Endpoint" />
+    <div v-for="n in numParams" :key="n" class="param-container">
+      <label>Parameter {{ n }}</label>
+      <input v-model="params[n - 1].key" type="text" placeholder="Parameter name" />
+      <input v-model="params[n - 1].value" type="text" placeholder="Parameter value" />
+    </div>
+    <button @click="addParam">Add Parameter</button>
     <button @click="makeRequest">Make Request</button>
     <p>{{ response }}</p>
     <h2>Request History</h2>
@@ -16,7 +22,6 @@
   </div>
 </template>
 
-
 <script>
 import axios from 'axios';
 
@@ -24,14 +29,31 @@ export default {
   data() {
     return {
       apiEndpoint: '',
+      numParams: 0,
+      params: [],
       response: null,
       requestHistory: []
     }
   },
   methods: {
+    addParam() {
+      this.numParams++;
+      this.params.push({
+        key: '',
+        value: ''
+      });
+    },
     async makeRequest() {
+      // build the params object from the user's input
+      const requestParams = {};
+      this.params.forEach(param => {
+        requestParams[param.key] = param.value;
+      });
+
       try {
-        const response = await axios.get('https://cors-anywhere.herokuapp.com/' + this.apiEndpoint);
+        const response = await axios.get('https://cors-anywhere.herokuapp.com/' + this.apiEndpoint, {
+          params: requestParams
+        });
         this.response = response.data;
         this.requestHistory.push({
           endpoint: this.apiEndpoint,
@@ -45,6 +67,4 @@ export default {
 }
 </script>
 
-<style scoped>
-</style>
 
